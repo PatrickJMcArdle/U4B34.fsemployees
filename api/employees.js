0962e9a2f07fcc96c690a403b237e76fce750289
PatrickJMcArdle <patrickjmcardle1@gmail.com>
2025-07-25 13:00:43 -0500

    partly done, getting lunch

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,13 @@
+import { getEmployees } from "#db/queries/employees";
 import express from "express";
 const router = express.Router();
 export default router;
 
 // TODO: this file!
+
+router.route("/")
+.get(async (req, res) => {
+  const employees = getEmployees();
+  res.send(employees)
+})
+.post
--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+import router from "#api/employees";
 import express from "express";
 const app = express();
 export default app;
 
 // TODO: this file!
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.route("/").get((req, res) => {
+  res.send("Welcome to the Fullstack Employees API.")
+})
+
+app.use("/employees", router);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong :(");
+});

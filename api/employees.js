aa7578a4eecd66c343b3fb3fe57551272bf5e410
PatrickJMcArdle <patrickjmcardle1@gmail.com>
2025-07-27 20:14:21 -0500

    done

--- api/employees.js
+++ api/employees.js
@@ -1,4 +1,4 @@
-import { getEmployees } from "#db/queries/employees";
+import { createEmployee, deleteEmployee, getEmployee, getEmployees, updateEmployee } from "#db/queries/employees";
 import express from "express";
 const router = express.Router();
 export default router;
@@ -7,7 +7,81 @@ export default router;
 
 router.route("/")
 .get(async (req, res) => {
-  const employees = getEmployees();
+  const employees = await getEmployees();
   res.send(employees)
 })
-.post
+.post(async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body required.");
+
+  const { name, birthday, salary } = req.body;
+
+  if (!name || !birthday || !salary) {
+    return res
+      .status(400)
+      .send("Missing required fields: name, birthday, salary");
+  }
+  const newEmployee = await createEmployee({name, birthday, salary});
+  res.status(201).json(newEmployee);
+});
+
+router.route("/:id")
+.get(async (req, res) => {
+  const {id} = req.params;
+  if(!/^\d+$/.test(id)) {
+    return res
+      .status(400)
+      .send("ID must be a positive integer");
+  }
+  const employee = await getEmployee(id)
+  if(!employee) {
+    return res
+      .status(404)
+      .send("There is no employee with that ID");
+  }
+  res.send(employee)
+})
+.delete(async (req, res) => {
+  const {id} = req.params;
+  if(!/^\d+$/.test(id)) {
+    return res
+      .status(400)
+      .send("ID must be a positive integer");
+  }
+  const employee = await deleteEmployee(id)
+  if(!employee) {
+    return res
+      .status(404)
+      .send("There is no employee with that ID");
+  }
+  res.sendStatus(204)
+})
+.put(async (req, res) => {
+  if(!req.body) {
+    return res
+    .status(400)
+    .send("Request body required.")
+  }
+  const {id} = req.params
+  const {
+    name,
+    birthday,
+    salary
+  } = req.body;
+  if (!name || !birthday || !salary) {
+    return res
+      .status(400)
+      .send("Missing required fields: name, birthday, salary");
+  }
+  if(!/^\d+$/.test(id)) {
+    return res
+      .status(400)
+      .send("ID must be a positive integer");
+  };
+  const employee = await updateEmployee({ id, name, birthday, salary })
+  if(!employee) {
+    return res
+      .status(404)
+      .send("There is no employee with that ID");
+  }
+  res.send(employee);
+})
